1f6445efa668b583863d9d2044fcffeed4ed2571
not-varram <varshithsatti18@gmail.com>
2024-10-24 04:55:52 -0700

    fix(heroku): fix to try to get it to work with herkou

--- server.js
+++ server.js
@@ -1,24 +1,31 @@
-// server.js
 "use strict";
 
-const path = require("path");
-const AutoLoad = require("@fastify/autoload");
-require("dotenv").config();
+const fastify = require('fastify')({ logger: true });
+const path = require('path');
+const AutoLoad = require('@fastify/autoload');
+require('dotenv').config();
 
-module.exports = async function (fastify, opts) {
-  // Register plugins
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, "plugins"),
-    options: Object.assign({}, opts),
-  });
+async function start() {
+  try {
+    // Register plugins
+    fastify.register(AutoLoad, {
+      dir: path.join(__dirname, 'plugins'),
+      options: {}
+    });
 
-  // Register routes
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, "routes"),
-    options: Object.assign({}, opts),
-  });
-};
+    // Register routes
+    fastify.register(AutoLoad, {
+      dir: path.join(__dirname, 'routes'),
+      options: {}
+    });
 
-module.exports.options = {
-  trustProxy: true,
-};
+    // Start the server
+    await fastify.listen({ port: process.env.PORT || 3000, host: '0.0.0.0' });
+    console.log(`Server listening on ${fastify.server.address().port}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+}
+
+start();
